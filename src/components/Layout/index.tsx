2e1780c990b512bdc392a662a3a40209447c748d
anhnhu <anhnt@subwallet.app>
2023-11-08 12:09:41 +0700

    [issue-6]: update github action create .env

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -9,6 +9,8 @@ import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 import "./layout.css"
+import {AppStateProvider} from "../../providers/AppStateProvider";
+import NotificationProvider from "../../providers/NotificationProvider";
 import {ThemeProvider} from "../../providers/ThemeProvider";
 interface Props {
     children: React.ReactNode
@@ -26,7 +28,11 @@ const Layout = ({ children }: Props) => {
 
   return (
     <ThemeProvider>
-        {children}
+        <NotificationProvider>
+          <AppStateProvider>
+              {children}
+          </AppStateProvider>
+      </NotificationProvider>
     </ThemeProvider>
   )
 }
